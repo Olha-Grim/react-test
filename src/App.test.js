4d7e8293018ab39dcd5b070a22024de968a03744
Olha-Grim <olha.hrymchak@prea.ws>
2021-07-26 13:28:11 +0300

    search variants and work with promise

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,11 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-  it("render", () => {
+  it("render", async() => {
     render(<App />);
     screen.debug();
-    expect(screen.getByText(/Search:/i)).toBeInTheDocument();
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
-    expect(screen.getByAltText(/search image/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/search text/i)).toBeInTheDocument();
-    expect(screen.getByDisplayValue('')).toBeInTheDocument();
-    
+    expect(screen.queryByText(/Logged in as/i)).toBeNull();
+    expect( await screen.findByText(/Logged in as/i)).toBeInTheDocument();
+  
   });
 });
