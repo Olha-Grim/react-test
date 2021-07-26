6c1f426a777beef0b6cffc98968ccf198ced0e30
Olha-Grim <olha.hrymchak@prea.ws>
2021-07-26 17:00:58 +0300

    fireEvent and how use

--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,40 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-/*
-Search variants:
-  getBy:                    queryby:                    findBy:
-- getByText               - queryByText               - findByText
-- getByRole               - queryByRole               - findByRole
-- getByLabelText          - queryByLabelText          - findByLabelText
-- getByPlaceholderText    - queryByPlaceholderText    - findByPlaceholderText
-- getByAltText            - queryByAltText            - findByAltText
-- getByDisplayValue       - queryByDisplayValue       - findByDisplayValue
-- getAllBy                - queryAllBy                - findAllBy
-*/
-
-/*
-Assertive Functions:
-- toBeDisabled            - toBeEnabled               - toBeEmpty
-- toBeEmptyDOMElement     - toBeInTheDocument         - toBeInvalid
-- toBeRequired            - toBeValid                 - toBeVisible
-- toContainElement        - toContainHTML             - toHaveAttribute
-- toHaveClass             - toHaveFocus               - toHaveFormValues
-- toHaveStyle             - toHaveTextContent         - toHaveValue
-- toHaveDisplayValue      - toBeChecked               - toBePartiallyChecked
-- toHaveDescription
-*/
-
 describe("App", () => {
-  it("renders App component", async () => {
+  test("renders App component", async () => {
     render(<App />);
-    // expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
-    expect(screen.queryByText(/Logged in as/)).toBeNull();
-    screen.debug();
-    expect(await screen.findByText(/Logged in as/)).toBeInTheDocument();
-    screen.debug();
-    // Assertive Functions Examples:
-    expect(screen.getByAltText(/search image/)).toHaveClass("image");
-    expect(screen.getByLabelText(/search/i)).not.toBeRequired();
-    expect(screen.getByLabelText(/search/i)).toBeEmpty();
-    expect(screen.getByLabelText(/search/i)).toHaveAttribute("id");
+    await screen.findByText(/Logged in as/i);
+    expect(screen.queryByText(/Searches for React/i)).toBeNull();
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "React" },
+    });
+    expect(screen.queryByText(/Searches for React/i)).toBeInTheDocument();
   });
+});
+
+describe("event", () => {
+  it("checkbox click", () => {
+    const handleChange = jest.fn();
+    const { container } = render(
+      <input type="checkbox" onChange={handleChange} />
+    );
+    const checkbox = container.firstChild;
+    expect(checkbox).not.toBeChecked();
+    fireEvent.click(checkbox);
+    // expect(handleChange).toHaveBeenCalledTimes(1);
+    expect(checkbox).toBeChecked();
+  });
+
+  it("input focus"),
+    () => {
+      const { getByTestId } = render(
+        <input type="text" data-testid="simple-input" />
+      );
+      const input = getByTestId("simple-input");
+      expect(input).not.toHaveFocus;
+      input.focus();
+      expect(input).toHaveFocus;
+    };
 });
