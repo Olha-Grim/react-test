ae90ff9ddaf6ae2fbcafd920b7cdafbf32144431
Olha-Grim <olha.hrymchak@prea.ws>
2021-07-26 13:09:50 +0300

    type of search

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("render", () => {
+    render(<App />);
+    screen.debug();
+    expect(screen.getByText(/Search:/i)).toBeInTheDocument();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+    expect(screen.getByAltText(/search image/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/search text/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+    
+  });
 });
